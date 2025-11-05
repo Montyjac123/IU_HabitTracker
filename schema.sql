60d883e66261460c34b00ddcdd2e905c17f408da
Montyjac123 <montanajacobs0@gmail.com>
2025-11-05 11:59:59 +0100

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -1,1 +1,16 @@
+CREATE TABLE IF NOT EXISTS habits (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT NOT NULL,
+    periodicity TEXT NOT NULL,
+    created_at TEXT NOT NULL,
+    active INTEGER NOT NULL,
+    notes TEXT
+);
+
+CREATE TABLE IF NOT EXISTS completions (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    habit_id INTEGER NOT NULL,
+    completion_date TEXT NOT NULL,
+    FOREIGN KEY (habit_id) REFERENCES habits (id) ON DELETE CASCADE
+);
 
